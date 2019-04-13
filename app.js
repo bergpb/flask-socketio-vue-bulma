f6a48e7b8fd7f1b6f095abdb828c7b0f16f79615
bergpb <lindembergdepaulo@gmail.com>
2019-04-13 16:05:14 -0300

    removed unused code and packages updated

--- app.js
+++
@@ -1,5 +0,0 @@
-var socket = io.connect('http://' + document.domain + ':' + location.port);
-socket.on('connect', function() {
-    // we emit a connected message to let knwo the client that we are connected.
-    socket.emit('client_connected', {data: 'New client!'});
-});
